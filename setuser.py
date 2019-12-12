e242808e91e5ed5aa845d734dd3de1cc7fd52aa0
吉川匠 <yo@yos-MacBook-puro.local>
2019-12-12 16:26:19 +0900

    setup改善

--- setuser.py
+++ setuser.py
@@ -1,6 +1,5 @@
 import Tkinter as tk
 import tkMessageBox
-import setting as Seting
 import math
 import numpy as np
 import time
@@ -14,70 +13,96 @@ from hx711 import HX711
 class SetUser(tk.Frame):
     def __init__(self,user, master=None, **kwargs):
         tk.Frame.__init__(self, master, **kwargs)
-        master.title("POSTURE RIGHTING")
+        self.master.title("POSTURE RIGHTING")
 
         #lbl = tk.Label(self, text="Please posture righting", height=5, font=("Migu 1M",20))
         #lbl.pack()
 
-        f_referenceUnit = 2.9
-	b_referenceUnit = 1
-	f_hx = HX711(5, 6) 
-	b_hx = HX711(23,24) 
-	f_hx.set_reading_format("MSB", "MSB") 
-	b_hx.set_reading_format("MSB", "MSB") 
+        self.refUnit = [2.9,1]
+	self.f_hx = HX711(5, 6)
+	self.f_hx.set_reading_format("MSB", "MSB")
+        self.b_hx = HX711(23,24)
+	self.b_hx.set_reading_format("MSB", "MSB")
  
-	f_hx.set_reference_unit(f_referenceUnit) 
-	b_hx.set_reference_unit(b_referenceUnit) 
+	self.f_hx.set_reference_unit(self.refUnit[0]) 
+	self.b_hx.set_reference_unit(self.refUnit[1]) 
  
-	f_hx.reset() 
-	b_hx.reset() 
+	self.f_hx.reset() 
+	self.b_hx.reset() 
  
-	f_hx.tare() 
-	b_hx.tare() 
+	self.f_hx.tare() 
+	self.b_hx.tare() 
  
-	print("Tare done! Add weight now...") 
- 
-	keisu = 600 
-        posture = ["normal","slouch","warp"] 
-        wari=[]
+	self.keisu = 100 
+        
+        self.create_widget()
 
-        for i in range(len(posture)):
-	    try:
-                pop = tkMessageBox.showinfo("POSTURE RIGHTING","Please posture "+posture[i])
-                lbl = tk.Label(self, text="Please posture "+posture[i], height=5, font=("Migu 1M",20))
-                lbl.pack()
-       
-		plot_count = 0 
-		f_list = []
-		b_list = []
-		while plot_count<keisu :
-		    f_arg = abs(f_hx.get_weight(1))
-		    b_arg = abs(b_hx.get_weight(1))
-		    f_list.append(f_arg)
-		    b_list.append(b_arg)
+    def create_widget(self):
+        btn = tk.Button(master=self,
+                text="Back",
+                width=5,
+                bg = "#00a4e4",
+                fg = "#ffffff",
+                command=self.master.backToStart)
+        btn.pack(anchor=tk.NW)
 
-		    print("front_roadcell:",f_list[plot_count])
-		    print("back_roadcell:",b_list[plot_count])
-		    plot_count += 1
+        lbl = tk.Label(master=self, text="Please select posture", height=5, font=("Migu 1M",20))
+        lbl.pack()
 
-		    f_hx.power_down()
-		    b_hx.power_down()
-		    f_hx.power_up()
-		    b_hx.power_up()
-	    except :
-		pass
-	    finally :
-		f_ave = np.mean(f_list)
-		b_ave = np.mean(b_list)
-		if b_ave>f_ave:
-		    wari.append((b_ave-f_ave)/b_ave)
-                elif b_ave<f_ave:
-                    wari.append(-1*(f_ave-b_ave)/f_ave)
-                else:
-                    wari.append(0)
+        Sbtn = tk.Button(master=self,
+                width=5,
+                bg = "#00a4e4",
+                text="Slouch",
+                fg = "#ffffff",
+                command=self.setPosturelam(0))
+        Sbtn.pack()
 
-                lbl.pack_foget()
-        Setting.users[user] = wari
+        Wbtn = tk.Button(master=self,
+                text="Warp",
+                width=5,
+                bg = "#00a4e4",
+                fg = "#ffffff",
+                command=self.setPosturelam(1))
+        Wbtn.pack()
+    def setPosturelam(self,pos):
+        return lambda :setPosture(pos)
+
+    def setPosture(self,pos):
+    try:
+	lbl = tk.Label(self, text="Please posture "+posture[i], height=5, font=("Migu 1M",20))
+	lbl.pack()
+
+        plot_count = 0 
+	f_list = []
+	b_list = []
+	while plot_count<self.keisu :
+	    f_arg = abs(self.f_hx.get_weight(1))
+	    b_arg = abs(self.b_hx.get_weight(1))
+	    f_list.append(f_arg)
+	    b_list.append(b_arg)
+
+	    print("front_roadcell:",f_list[plot_count])
+	    print("back_roadcell:",b_list[plot_count])
+	    plot_count += 1
+	    print(plot_count)
+	    self.f_hx.power_down()
+	    self.b_hx.power_down()
+	    self.f_hx.power_up()
+	    self.b_hx.power_up()
+    except :
+	pass
+    finally :
+	f_ave = np.mean(f_list)
+	b_ave = np.mean(b_list)
+	if b_ave>f_ave:
+	    wari = ((b_ave-f_ave)/b_ave)
+	elif b_ave<f_ave:
+	    wari = (-1*(f_ave-b_ave)/f_ave)
+	else:
+	    wari = 0
+
+	lbl.pack_forget()
+        self.master.users[pos] = wari
 	cleanAndExit()
         self.master.backToStart()
 
